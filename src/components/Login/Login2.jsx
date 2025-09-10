b5806c83e0aeb170e60bb9f0e65d8eb0c5ce3edc
Thimita20 <thimirapanditha20@gmail.com>
2025-09-10 15:00:24 +0530

    Refactor Login component: replace avatar image with animated SVG icon and enhance modal styles

--- src/components/Login/Login2.jsx
+++ src/components/Login/Login2.jsx
@@ -85,11 +85,33 @@ function Login({ closeLoginModal, setUser, open }) {  // Added 'open' prop
           onSubmit={handleSubmit}
         >
           <div className="imgcontainer">
-            <img
-              src="https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-              alt="Avatar"
-              className="avatar"
-            />
+            <div className="login-icon">
+              <svg 
+                width="80" 
+                height="80" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                xmlns="http://www.w3.org/2000/svg"
+                className="animated-login-icon"
+              >
+                <circle 
+                  cx="12" 
+                  cy="8" 
+                  r="3" 
+                  stroke="currentColor" 
+                  strokeWidth="2" 
+                  fill="none"
+                  className="user-head"
+                />
+                <path 
+                  d="M6 21v-2a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v2" 
+                  stroke="currentColor" 
+                  strokeWidth="2" 
+                  fill="none"
+                  className="user-body"
+                />
+              </svg>
+            </div>
           </div>
 
           <div className="container">
